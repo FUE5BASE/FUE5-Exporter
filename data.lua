b96038563080a0ef9b72378a90b31cf47d138d10
LianJordaan <lianjordaan30052005@gmail.com>
2024-10-27 09:23:11 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -11,34 +11,34 @@ data:extend(
 			flags = { "icon" }
 		}
 	},
-	{
-		type = "selection-tool",
-		name = "FUE5Exporter__export-selector",
-		icon = "__base__/graphics/icons/signal/signal_E.png",
-		icon_size = 64,
-		subgroup = "tool",
-		stack_size = 1,
-		stackable = false,
-		draw_label_for_cursor_render = true,
-		selection_color = { r = 0, g = 0, b = 1 },
-		alt_selection_color = { r = 1, g = 0, b = 0 },
-		flags = { "only-in-cursor" },
-		selection_mode = { "any-entity" },
-		alt_selection_mode = { "any-entity" },
-		selection_cursor_box_type = "entity",
-		alt_selection_cursor_box_type = "entity",
-		entity_filter_mode = "blacklist",
-		entity_type_filters = {
-			"character",
-			"resource",
-			"spider-leg"
-		},
-		alt_entity_filter_mode = "blacklist",
-		alt_entity_type_filters = {
-			"character",
-			"resource",
-			"spider-leg"
-		}
-	}
+    {
+        type = "selection-tool",
+        name = "FUE5Exporter__export-selector",
+        icon = "__base__/graphics/icons/signal/signal_E.png",
+        icon_size = 64,
+        subgroup = "tool",
+        stack_size = 1,
+        stackable = false,
+        draw_label_for_cursor_render = true,
+        
+        -- New properties introduced in Factorio 2.0
+        select = {
+            selection_color = { r = 0, g = 0, b = 1 },
+            selection_cursor_box_type = "entity",
+            entity_filter_mode = "blacklist",
+            entity_type_filters = { "character", "resource", "spider-leg" }
+        },
+        alt_select = {
+            selection_color = { r = 1, g = 0, b = 0 },
+            selection_cursor_box_type = "entity",
+            entity_filter_mode = "blacklist",
+            entity_type_filters = { "character", "resource", "spider-leg" }
+        },
+        
+        -- Optional properties
+        always_include_tiles = false,
+        mouse_cursor = "selection-tool-cursor",
+        skip_fog_of_war = false
+    }
 })
 
